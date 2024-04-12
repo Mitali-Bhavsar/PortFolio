8a06020143edd857d64b624a4644400d57a9cc66
Mitali-Bhavsar <mitali.bodkhe@gmail.com>
2024-04-12 20:21:10 +0530

    about.js updated

--- src/webpages/About.js
+++ src/webpages/About.js
@@ -6,7 +6,7 @@ const About = () => {
       <div className='about-text'>
     <span className="text1"> About</span>
     <span className="text2">
-    Experienced UI Developer with passion for creating attractive and interactive websites meeting customer needs
+    Having 2+ years of experince in developing.Experienced UI Developer with passion for creating attractive and interactive websites meeting customer needs
 and exceeding expectations. Focused on creating clean, robust code with exceptional security. 
     </span>
     </div>
