a5dc96cd1e33aa272b6800ea05cee39f338ac21a
Mitali-Bhavsar <mitali.bodkhe@gmail.com>
2024-07-29 18:55:50 +0530

    skills updated

--- src/webpages/Skills.js
+++ src/webpages/Skills.js
@@ -15,7 +15,7 @@ const Skills = () => {
 			<span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>
        </div>
        <div className='container'>
-        <div className='row'>
+        <div className='row mb-5rem'>
           <div className='col-md-6'>
               <p className='s-skills' data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000"><i className="bi bi-filetype-html px-3"></i>HTML5</p>
           </div>
@@ -24,17 +24,28 @@ const Skills = () => {
           </div>
        
           <div className='col-md-6'>
-              <p className='s-skills' data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1200"><i className="bi bi-bootstrap px-3"></i>BootStrap</p>
+              <p className='s-skills' data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1200"><i className="bi bi-bootstrap px-3"></i>BootStrap 5</p>
           </div>
           <div className='col-md-6'>
             <p className='s-skills' data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1200"><i className="bi bi-filetype-js px-3"></i>JavaScript</p>
           </div>
-       
+          <div className='col-md-6'>
+            <p className='s-skills' data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1200"><i className="bi bi-filetype-js px-3"></i>ES6</p>
+          </div>         
+          <div className='col-md-6'>
+            <p className='s-skills' data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1400"><i className="bi bi-braces px-3"></i>jQuery</p>
+          </div>
           <div className='col-md-6'>
               <p className='s-skills' data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1400"><i className="bi bi-filetype-jsx px-3"></i>React JS</p>
           </div>
           <div className='col-md-6'>
-            <p className='s-skills' data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1400"><i className="bi bi-braces px-3"></i>jQuery</p>
+              <p className='s-skills' data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1400"><i className="bi bi-shop-window px-3"></i>Redux</p>
+          </div>
+          <div className='col-md-6'>
+              <p className='s-skills' data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1400"><i className="bi bi-collection px-3"></i>Material UI</p>
+          </div>
+          <div className='col-md-6'>
+              <p className='s-skills' data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1400"><i className="bi bi-collection px-3"></i>Semantic UI</p>
           </div>
        </div> 
 
