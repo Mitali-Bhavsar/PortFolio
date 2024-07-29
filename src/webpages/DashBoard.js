f3f9bbe0a5ea524df09dad497e130c1d5bcfc5bc
Mitali-Bhavsar <mitali.bodkhe@gmail.com>
2024-07-29 18:14:46 +0530

    Dashboard Page updated

--- src/webpages/DashBoard.js
+++ src/webpages/DashBoard.js
@@ -12,6 +12,7 @@ const DashBoard = () => {
                       <ul className='social-medial'>
                         <li><NavLink to="https://www.linkedin.com/in/mitali-bhavsar0202" target="_blank" title="LinkedIN" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-original-title="LinkedIN"><i className="bi bi-linkedin"></i></NavLink></li>
                         <li><NavLink to="https://github.com/Mitali-Bhavsar" title="GitHub" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-original-title="GitHub"><i className="bi bi-github"></i></NavLink></li>
+                        <li><NavLink to="https://app.netlify.com/teams/mitali-bodkhe/sites" title="Netlify" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-original-title="Netlify"><i className="bi bi-globe2"></i></NavLink></li>
                       </ul>
                   </div>
                  
